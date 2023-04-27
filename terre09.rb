e5a8002079e7017f8f7356da0295b2f5f095502e
kyby91 <julienmoullec@yahoo.fr>
2023-04-28 00:10:14 +0200

    terre09 end

--- terre09.rb
+++ terre09.rb
@@ -1,20 +1,24 @@
 #racine carré
 
 def calculRacine(nombre)
-    result = nombre ** 0.5
-    puts result
+    if nombre < 0
+        puts 'inférieure à 0'
+    else
+        result = nombre ** 0.5
+        puts result
+    end
 end
 
 def verifyArg (nombre)
     if  nombre != nil #vérifier si arg présent
-        if nombre.scan(/^\d+$/).any? #vérifier si uniquement chiffre / chiffre entier / chiffre positif
+        if nombre.scan(/^\-?\d+$/).any? #vérifier si uniquement chiffre / chiffre entier / chiffre positif
             x = nombre.to_i
             calculRacine(x)
         else            
             puts "Caractère autre qu'un chiffre"
         end
     else
-        puts 'Il manque un ou plusieurs arguments'
+        puts 'Il manque un argument'
     end
 end
 
